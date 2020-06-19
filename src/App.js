e839850de4cd64a539f645b9f2c3fb172e7d555a
Crista Lopes <lopes@ics.uci.edu>
2020-06-18 17:18:01 -0700

    Add admin/rooms

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import VideoChat from "./components/VideoChat";
 //
 import LiveVideosList from "./components/Admin/LiveVideos";
 import Registrations from "./components/Admin/Registrations";
+import Rooms from "./components/Admin/Program/Rooms";
 // import EditUser from "./components/Admin/Users/EditUser";
 // import ChannelList from "./components/ChannelList";
 //import Chat from "./components/Chat";
@@ -202,6 +203,7 @@ class App extends Component {
             {/*<Route exact path='/admin/users/edit/:userID' component={withAuthentication(EditUser)} />*/}
             <Route exact path='/admin/livevideos' component={LiveVideosList}/>
             <Route exact path='/admin/registrations' component={Registrations}/>
+            <Route exact path='/admin/program/rooms' component={Rooms}/>
         </div>)
     }
 
