a3b478d1f1baab3768ab27fcc4b2e04e71fa63a3
Crista Lopes <lopes@ics.uci.edu>
2020-06-27 12:49:42 -0700

    Put all sessions under the same root menu. Change the frame for Zoom sessions.

--- src/components/linkMenu.js
+++ src/components/linkMenu.js
@@ -71,7 +71,7 @@ class LinkMenu extends React.Component {
             userTools =
                 [
                     <SubMenu key="/live" title={<span><VideoCameraOutlined/><span>Sessions</span></span>}>
-                        <Menu.Item key='/live/now' icon={<VideoCameraTwoTone twoToneColor="#eb2f96"/>}><NavLink to="/live/now">Live Sessions</NavLink></Menu.Item>,
+                        <Menu.Item key='/live/now' icon={<VideoCameraTwoTone twoToneColor="red"/>}><NavLink to="/live/now">Live Sessions</NavLink></Menu.Item>,
                         <Menu.Item key='/live/past' icon={<YoutubeOutlined/>}><NavLink to="/live/past">Past Sessions</NavLink></Menu.Item>
                     </SubMenu>,
 
