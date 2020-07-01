4dbaa5aee40def7a160368293d1d4df348908e20
Crista Lopes <lopes@ics.uci.edu>
2020-07-01 10:35:33 -0700

    CHINA WORKS!

--- src/components/LiveStreaming/utils.js
+++ src/components/LiveStreaming/utils.js
@@ -17,19 +17,20 @@ const LiveVideoSourceMappings = {
         }
     },
     "iQIYI" : {
-        url : "http://m3u8live.video.iqiyi.com/tslive/liveugc/",
+        url : "https://live.iqiyi.com/", //https://m-live.iqiyi.com/177383?source=
         vars : {
-            pv : "0.2",
-            atype : "qiyi",
-            qd_tvid: "3104576123",
-            qd_vipres: "0",
-            qd_scc: "35c08a7820bd1a04d83bc8c8aa1d1003",
-            qd_sc: "f3b55e56a4b2b155fed75e00a60c26be",
-            qd_src: "01010031010000000000",
-            qd_ip: "72603d2c",
-            qd_uid: "0",
-            qd_tm: "1591621238950",
-            qd_vip: "0"
+            // pv : "0.2",
+            // source: "localhost"
+            // atype : "qiyi",
+            // qd_tvid: "3104576123",
+            // qd_vipres: "0",
+            // qd_scc: "35c08a7820bd1a04d83bc8c8aa1d1003",
+            // qd_sc: "f3b55e56a4b2b155fed75e00a60c26be",
+            // qd_src: "01010031010000000000",
+            // qd_ip: "72603d2c",
+            // qd_uid: "0",
+            // qd_tm: "1591621238950",
+            // qd_vip: "0"
         }
     },
     "ZoomUS" : {
