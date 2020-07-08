c0abc1f73a7d6cc27fdf124f3bc6a675f5a6422b
Benjamin Pierce <bcpierce00t@gmail.com>
2020-07-08 10:36:26 -0400

    Add a "Send a message" button to the userStatusDisplay popover

--- src/components/Lobby/UserStatusDisplay.js
+++ src/components/Lobby/UserStatusDisplay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Avatar, Badge, Popover, Skeleton, Tag, Tooltip} from "antd";
+import {Avatar, Badge, Button, Popover, Skeleton, Tag, Tooltip} from "antd";
 import {AuthUserContext} from "../Session";
 import {withRouter} from "react-router-dom";
 
@@ -129,12 +129,13 @@ class UserStatusDisplay extends React.Component{
                     {this.state.profile.get("webpage")}</a>
                 </div>;
         }
+        let dmButton = <Button type="primary" onClick={onClick}>Send a message to {this.state.profile.get("displayName")}</Button>
         let firstLine =
             <div>
               {dntWaiver}
             </div>;
         // BCP: And this needs a bit more vertical spacing between non-empty elements too:
-        let popoverContent = <div className="userPopover"> {firstLine} {bio} {webpage} </div>;
+        let popoverContent = <div className="userPopover"> {firstLine} {bio} {webpage} {dmButton} </div>;
 /*
         let popoverContent = <span></span>
         // BCP: Not clear to me why we were treating these so popovers differently
