clowdr-app/clowdr-web-app

commit e3b7b502f4c5b203a26e1465b99cdccf6323061b

change Admin/ProgramSummary to TS

src/components/Admin/Program/ProgramSummary/ProgramSummary.tsx
@@ -1,32 +1,57 @@
 import React from 'react';
-import {Button, message, Popconfirm, Select, Space, Spin, Table, Tabs, Upload} from "antd";
+import {Button, message, Popconfirm, Select, Space, Spin, Table, Tabs, Upload, } from "antd";
 import Parse from "parse";
 import {AuthUserContext} from "../../../Session";
 
 import {UploadOutlined} from '@ant-design/icons';
+import { ClowdrAppState } from "../../../../ClowdrTypes";
+import { UploadChangeParam, RcFile } from 'antd/lib/upload/interface';
+
+interface ProgramSummaryProps {
+    auth: ClowdrAppState,
+}
+
+interface ProgramSummaryState {
+    loading: boolean,
+    visible: boolean,
+    deleteLoading: boolean,
+    ProgramSessions: Parse.Object[],
+    ProgramItems: Parse.Object[],
+    ProgramTracks: Parse.Object[],
+    ProgramRooms: Parse.Object[],
+    ProgramPersons: Parse.Object[],
+}
 
 const { Option } = Select;
 
-const {TabPane} = Tabs;
-const IconText = ({icon, text}) => (
-    <Space>
-        {React.createElement(icon)}
-        {text}
-    </Space>
-);
+// const {TabPane} = Tabs;
+// const IconText = ({icon, text}) => (
+//     <Space>
+//         {React.createElement(icon)}
+//         {text}
+//     </Space>
+// );
+//TS: Not used anywhere
 
-
-class ProgramSummary extends React.Component {
-    constructor(props) {
+class ProgramSummary extends React.Component<ProgramSummaryProps, ProgramSummaryState> {
+    currentConference: any;
+    constructor(props: ProgramSummaryProps) {
         super(props); // has props.auth
         this.state = {
             loading: true,
+            visible: true,
+            deleteLoading: false,
+            ProgramSessions: [],
+            ProgramItems: [],
+            ProgramTracks: [],
+            ProgramRooms: [],
+            ProgramPersons: [],
         };
         // this.currentConference = "XYZ";
         this.currentConference = this.props.auth.currentConference;
     }
 
-    onChange(info) {
+    onChange(info: UploadChangeParam) { 
         console.log("onChange " + info.file.status);
         if (info.file.status !== 'uploading') {
             console.log(info.file, info.fileList);
@@ -57,7 +82,7 @@ class ProgramSummary extends React.Component {
 
     }
 
-    beforeUpload(file, fileList) {
+    beforeUpload(file: RcFile) {
         const reader = new FileReader();
         reader.onload = () => {
             const data = {content: reader.result, conference: this.currentConference.id};
@@ -69,7 +94,7 @@ class ProgramSummary extends React.Component {
                 .catch(err => {
                     console.log('Upload failed: ' + err)
                     console.log(err);
-                    message.err("Error uploading program, please see console for debugging")
+                    message.error("Error uploading program, please see console for debugging")
                 });
         }
         reader.readAsText(file);
@@ -160,26 +185,26 @@ class ProgramSummary extends React.Component {
                         <UploadOutlined /> Click to upload program data
                     </Button>
                 </Upload>
-                <Popconfirm title="Are you sure you want to delete the entire program? This can't be undone." onConfirm={this.deleteProgram.bind(this)}><Button type="danger" loading={this.state.deleteLoading}>Delete entire program</Button></Popconfirm>
+                <Popconfirm title="Are you sure you want to delete the entire program? This can't be undone." onConfirm={this.deleteProgram.bind(this)}><Button type="primary" danger loading={this.state.deleteLoading}>Delete entire program</Button></Popconfirm>
                 <Table
                     columns={columns} 
                     dataSource={counts}
                     rowKey={(r)=>(r.key)}
                     pagination={{ defaultPageSize: 500,
-                        pageSizeOptions: [10, 20, 50, 100, 500], 
-                        position: ['topRight', 'bottomRight']}}/>
+                        pageSizeOptions: ['10', '20', '50', '100', '500'], 
+                        position: ['topRight', 'bottomRight']
+                    }}/>
             </div>
         )
     }
 }
 
-const AuthConsumer = (props) => (
-            <AuthUserContext.Consumer>
-                {value => (
-                    <ProgramSummary {...props} auth={value} />
-                )}
-            </AuthUserContext.Consumer>
-
+const AuthConsumer = (props: ProgramSummaryProps) => (
+    <AuthUserContext.Consumer>
+        {value => (value == null ? <></> : //@ts-ignore
+            <ProgramSummary {...props} auth={value} />
+        )}
+    </AuthUserContext.Consumer>
 );
 
 export default AuthConsumer;

src/components/Admin/Program/ProgramSummary/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ProgramSummary from "./ProgramSummary";
 import AuthUserContext from "../../../Session/context";
+import { ClowdrAppState } from "../../../../ClowdrTypes";
 
-const ProgramAdminArea = (props) => (
+interface ProgramSummaryProps {
+    auth: ClowdrAppState,
+}
+
+const ProgramAdminArea = (props: ProgramSummaryProps) => (
     <AuthUserContext.Consumer>
-        {value => (
+        {value => (value == null ? <></> : //@ts-ignore
             <ProgramSummary {...props} auth={value}/>
         )}
     </AuthUserContext.Consumer>

commit 580654ed18f1e29439a1d74462fff56ca770ec30

Fix type checking errors introduced by e3b7b502f4c5b203a26e1465b99cdccf6323061b

src/components/Admin/Program/ProgramSummary/ProgramSummary.tsx
@@ -60,16 +60,15 @@ class ProgramSummary extends React.Component<ProgramSummaryProps, ProgramSummary
 
     async componentDidMount() {
         let program = await this.props.auth.programCache.getEntireProgram(this);
-        program.loading = false;
-        this.setState(program);
+        this.setState({...program, loading: false});
     }
     
     componentWillUnmount() {
-        this.props.auth.programCache.cancelSubscription("ProgramItem", this);
-        this.props.auth.programCache.cancelSubscription("ProgramRoom", this);
-        this.props.auth.programCache.cancelSubscription("ProgramTrack", this);
-        this.props.auth.programCache.cancelSubscription("ProgramPerson", this);
-        this.props.auth.programCache.cancelSubscription("ProgramSession", this);
+        this.props.auth.programCache.cancelSubscription("ProgramItem", this, undefined);
+        this.props.auth.programCache.cancelSubscription("ProgramRoom", this, undefined);
+        this.props.auth.programCache.cancelSubscription("ProgramTrack", this, undefined);
+        this.props.auth.programCache.cancelSubscription("ProgramPerson", this, undefined);
+        this.props.auth.programCache.cancelSubscription("ProgramSession", this, undefined);
     }
 
     beforeUpload(file: RcFile) {