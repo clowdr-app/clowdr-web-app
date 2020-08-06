a95b6d420042486a83b0934326da6233f504f145
Benjamin Pierce <bcpierce00t@gmail.com>
2020-08-06 14:15:55 -0400

    Small question for Bingfei

--- src/components/Admin/Registrations/Registrations.tsx
+++ src/components/Admin/Registrations/Registrations.tsx
@@ -32,7 +32,7 @@ interface RegistrationSchema {
     country: string
 }
 
-function validateEmail(email: String) {
+function validateEmail(email: String) { // TS: Why String instead of string???
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
