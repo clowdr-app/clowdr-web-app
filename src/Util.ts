f452d9cc700d698de899f3430885f9756e06b8d7
Harrison Goldstein <hgoldstein95@gmail.com>
2020-08-29 13:46:11 -0400

    Removed YOUTUBE_API_KEY from Util
    It definitely shouldn't have been there anyway, and it isn't used anywhere.

--- src/Util.ts
+++ src/Util.ts
@@ -6,6 +6,3 @@ dotenv.config();
 export function assert(input: any): asserts input { // <-- not a typo
     if (!input) throw new Error('Assertion failed!');
 }
-
-// @Jon: Why does a module called Utils have an export default??
-export default { YOUTUBE_API_KEY: process.env.REACT_APP_YOUTUBE_API_KEY };
