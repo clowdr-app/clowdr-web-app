1e75804b5ad042ab36e9b72a0e024266bef27a46
Ed Nutting <ed@beyondrisc.com>
2020-08-29 23:49:35 +0100

    JS -> TS: SignOut component

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import * as ROUTES from '../../constants/routes';
 import { Parse } from "../parse/parse";
 import { AuthUserContext } from "../Session";
+import { MaybeParseUser, MaybeClowdrInstance } from "../../ClowdrTypes";
 
-class SignOut extends Component {
+interface SignOutProps {
+    history: string[];
+    refreshUser: (instance?: MaybeClowdrInstance, forceRefresh?: boolean) => Promise<MaybeParseUser>;
+}
+
+interface SignOutState {
+}
+
+class SignOut extends Component<SignOutProps, SignOutState> {
     componentDidMount() {
         Parse.User.logOut().then(() => {
             this.props.refreshUser(null, true).then(() => {
@@ -19,10 +28,10 @@ class SignOut extends Component {
     }
 }
 
-const AuthConsumer = (props) => (
+const AuthConsumer = (props: SignOutProps) => (
     <AuthUserContext.Consumer>
-        {value => (
-            <SignOut {...props} user={value.user} refreshUser={value.refreshUser} />
+        {value => (value == null ? <span>TODO: SignOut page when clowdrState is null.</span> :
+            <SignOut {...props} refreshUser={value.refreshUser} />
         )}
     </AuthUserContext.Consumer>
 );
