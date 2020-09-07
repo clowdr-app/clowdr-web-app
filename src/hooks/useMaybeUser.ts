5ba01f4e4e26513e9ae4abf802d5d2092c493fa7
Ed Nutting <ed@beyondrisc.com>
2020-09-08 00:57:21 +0100

    useMaybeUser: It's okay to return null

--- src/hooks/useMaybeUser.ts
+++ src/hooks/useMaybeUser.ts
@@ -1,10 +1,7 @@
 import { useContext } from 'react';
 import UserContext from '../contexts/UserContext';
 import { User } from '../classes/Data';
-import assert from 'assert';
 
-export default function useMaybeUser(): User {
-    let ctx = useContext(UserContext);
-    assert(ctx, "User should be defined.");
-    return ctx;
+export default function useMaybeUser(): User | null {
+    return useContext(UserContext);
 }
