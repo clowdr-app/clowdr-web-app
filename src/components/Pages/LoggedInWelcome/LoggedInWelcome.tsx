50afd1c9c4722e6a8f3f16b14181bb19883cacf8
Ed Nutting <ed@beyondrisc.com>
2020-09-17 01:37:19 +0100

    [feature]: Load logged-in-welcome-text from the DB.

--- src/components/Pages/LoggedInWelcome/LoggedInWelcome.tsx
+++ src/components/Pages/LoggedInWelcome/LoggedInWelcome.tsx
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useConference from "../../../hooks/useConference";
 import useDocTitle from "../../../hooks/useDocTitle";
 
 export default function LoggedInWelcome() {
     const conference = useConference();
+    const [contents, setContents] = useState<string>("You are logged in. Loading conference information...");
 
     const docTitle = useDocTitle();
     useEffect(() => {
         docTitle.set(conference.name);
-    });
+    }, [docTitle, conference]);
+
+    useEffect(() => {
+        conference.details.then(details => {
+            let text = details.find(x => x.key === "LOGGED_IN_TEXT")?.value;
+            if (text) {
+                setContents(text);
+            }
+        });
+    }, [conference]);
 
     return <section aria-labelledby="page-title" tabIndex={0}>
-        <p>You are logged in.</p>
+        <p>{contents}</p>
     </section>;
 }
