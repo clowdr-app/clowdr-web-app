f5aef8c7081e741b8356f2141a4855f046e874ff
Ross Gardiner <github@rossng.eu>
2020-09-20 18:30:01 +0100

    [not-found] Improve styling of NotFound.tsx component

--- src/components/Pages/NotFound/NotFound.tsx
+++ src/components/Pages/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useDocTitle from "../../../hooks/useDocTitle";
+import "./NotFound.scss";
 
-export default function ChatView() {
+export default function NotFound() {
     useDocTitle("Not Found");
-    return <>Page Not Found</>;
+    return <>
+        <div className="not-found">
+            <i className="fas fa-cat fa-5x icon"></i>
+            <h2>Sorry, we couldn't find that page.</h2>
+            <p>
+                If you keep seeing this error, please <Link to="/moderators">contact your conference organizers</Link>.
+        </p>
+        </div>
+    </>;
 }
