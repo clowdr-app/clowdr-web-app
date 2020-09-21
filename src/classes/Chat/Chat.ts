5f7e5e23907de44efe4d7c46e17415541cce7d36
Ed Nutting <github@ednutting.com>
2020-09-21 23:24:28 +0100

    [app] [chat]: Switch off the mockup Chat initialisation code.

--- src/classes/Chat/Chat.ts
+++ src/classes/Chat/Chat.ts
@@ -1,10 +1,12 @@
-import assert from "assert";
-import { Conference, ConferenceConfiguration, UserProfile } from "clowdr-db-schema/src/classes/DataLayer";
+import { Conference, UserProfile } from "clowdr-db-schema/src/classes/DataLayer";
 
 export default class Chat {
     private static chat: Chat | null = null;
 
     private _REACT_APP_TWILIO_CALLBACK_URL: string | null = null;
+
+    private initialisePromise: Promise<boolean> | null = null;
+    private teardownPromise: Promise<void> | null = null;
 
     constructor(
         private conference: Conference,
@@ -14,51 +16,81 @@ export default class Chat {
     }
 
     public async initialise(): Promise<boolean> {
-        let token = await this.getToken();
-        if (!token) {
-            // TODO: Use a debug logger
-            console.log("[Chat]: twilio token not obtained.");
-            return false;
+        if (!this.initialisePromise) {
+            this.initialisePromise = new Promise((resolve, reject) => {
+                reject("Chat initialisation not implemented.");
+            });
         }
-        return true;
+        else {
+            // TODO: Use logger
+            console.warn("Re-initialisation of chat without waiting for teardown.")
+        }
+
+        return this.initialisePromise;
+        // TODO: Put this in the Twilio client
+    //     let token = await this.getToken();
+    //     if (!token) {
+    //         // TODO: Use a debug logger
+    //         console.log("[Chat]: twilio token not obtained.");
+    //         return false;
+    //     }
+    //     return true;
     }
 
     public async teardown(): Promise<void> {
-
-    }
-
-    private async get_REACT_APP_TWILIO_CALLBACK_URL(): Promise<string> {
-        if (!this._REACT_APP_TWILIO_CALLBACK_URL) {
-            let results = await ConferenceConfiguration.getByKey("REACT_APP_TWILIO_CALLBACK_URL", this.conference.id);
-            assert(results.length, "Conference Twilio not configured.");
-            this._REACT_APP_TWILIO_CALLBACK_URL = results[0].value;
+        if (!this.teardownPromise) {
+            if (this.initialisePromise) {
+                this.teardownPromise = this.initialisePromise.then(() => {
+                    // TODO: Use logger
+                    console.log("Tore down chat client.");
+                    this.initialisePromise = null;
+                }).catch((err) => {
+                    // TODO: Use logger
+                    console.warn("Ignoring chat initialisation error as we're teraing down anyway.", err);
+                    this.initialisePromise = null;
+                });
+            }
+            else {
+                return Promise.resolve();
+            }
         }
-        return this._REACT_APP_TWILIO_CALLBACK_URL;
+
+        return this.teardownPromise ?? Promise.resolve();
     }
 
-    private async getToken(): Promise<string | null> {
-        // TODO: Move into Twilio client
+    // TODO: Put this in the Twilio client
+    // private async get_REACT_APP_TWILIO_CALLBACK_URL(): Promise<string> {
+    //     if (!this._REACT_APP_TWILIO_CALLBACK_URL) {
+    //         let results = await ConferenceConfiguration.getByKey("REACT_APP_TWILIO_CALLBACK_URL", this.conference.id);
+    //         assert(results.length, "Conference Twilio not configured.");
+    //         this._REACT_APP_TWILIO_CALLBACK_URL = results[0].value;
+    //     }
+    //     return this._REACT_APP_TWILIO_CALLBACK_URL;
+    // }
 
-        // TODO: Use a debug logger
-        console.log(`[Chat]: Fetching chat token for ${this.profile.displayName} (${this.profile.id}), ${this.conference.name} (${this.conference.id})`);
+    // private async getToken(): Promise<string | null> {
+    //     // TODO: Move into Twilio client
 
-        let callbackUrl = await this.get_REACT_APP_TWILIO_CALLBACK_URL();
-        const res = await fetch(
-            `${callbackUrl}/chat/token`,
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    identity: this.sessionToken,
-                    conference: this.conference.id
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-        let data = await res.json();
-        console.log(`[Chat]: Token obtained.`);
-        return data.token;
-    }
+    //     // TODO: Use a debug logger
+    //     console.log(`[Chat]: Fetching chat token for ${this.profile.displayName} (${this.profile.id}), ${this.conference.name} (${this.conference.id})`);
+
+    //     let callbackUrl = await this.get_REACT_APP_TWILIO_CALLBACK_URL();
+    //     const res = await fetch(
+    //         `${callbackUrl}/chat/token`,
+    //         {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 identity: this.sessionToken,
+    //                 conference: this.conference.id
+    //             }),
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         });
+    //     let data = await res.json();
+    //     console.log(`[Chat]: Token obtained.`);
+    //     return data.token;
+    // }
 
     public static async teardown() {
         Chat.chat?.teardown();
