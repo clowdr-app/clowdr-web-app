46908b4d674c188cb2fd44b668fe82d856e8df65
Ed Nutting <github@ednutting.com>
2020-09-23 00:13:27 +0100

    [logged-in-welcome]: Remove dead import

--- src/components/Pages/LoggedInWelcome/LoggedInWelcome.tsx
+++ src/components/Pages/LoggedInWelcome/LoggedInWelcome.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactMarkdown from 'react-markdown';
 import useConference from "../../../hooks/useConference";
 import useDocTitle from "../../../hooks/useDocTitle";
