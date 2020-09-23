88fb0cb786eb9c4a99dd74b709cb22166961d72e
Ed Nutting <github@ednutting.com>
2020-09-23 02:40:06 +0100

    [new-chat]: Writing down thoughts about how to implement it.

--- src/components/Pages/NewChat/NewChat.tsx
+++ src/components/Pages/NewChat/NewChat.tsx
@@ -27,6 +27,25 @@ export default function NewChat(props: Props) {
         return undefined;
     }, setDMUserProfile, [props.dmUserId]);
 
+    /**
+     * Elements that need to be on this page (as a MVP):
+     * 
+     *    * Who to invite to the chat (+ auto-fill for DM scenario)
+     *    * Public vs private selector
+     *    * Chat title (unless DM'ing)
+     * 
+     *    * Perhaps we need to put channel creation through parse to enforce the
+     *      rules of DMs? This requires turning off the createChannel permission
+     *      for service users/admins.
+     * 
+     *    * DM only if private selected, otherwise treat as a public group with
+     *      2 participants
+     *    * Generate a unique name: Perhaps `creatorUserId-${randomGUID}`
+     *      unless it's a DM, in which case `userId1-userId2`
+     *        (where userId1/2 are creator/target sorted alpha-numerically)
+     *    * For DMs, verify whether a channel already exists
+     */
+
     return <>{dmUserProfile
         ? <>You, {currentUserProfile.displayName}, wish to DM {dmUserProfile.displayName}?</>
         : <>You, {currentUserProfile.displayName}, shall DM no-one!</>
