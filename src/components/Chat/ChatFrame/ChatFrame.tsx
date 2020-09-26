3572c6f5f801bad32dced899676a906c60fc1c2d
Ed Nutting <github@ednutting.com>
2020-09-26 01:20:07 +0100

    [chat frame]: Example catch unauthorized to send message error

--- src/components/Chat/ChatFrame/ChatFrame.tsx
+++ src/components/Chat/ChatFrame/ChatFrame.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import useLogger from "../../../hooks/useLogger";
 import useMaybeChat from "../../../hooks/useMaybeChat";
 import MessageList from "../MessageList/MessageList";
 import "./ChatFrame.scss";
@@ -9,17 +10,34 @@ interface Props {
 
 export default function ChatFrame(props: Props) {
     const mChat = useMaybeChat();
+    const logger = useLogger("Chat Frame");
 
-    function sendMessage(ev: React.KeyboardEvent<HTMLInputElement>) {
+    async function sendMessage(ev: React.KeyboardEvent<HTMLInputElement>) {
         if (ev.key === "Enter") {
             if (mChat) {
                 ev.preventDefault();
                 ev.stopPropagation();
 
-                mChat.sendMessage(props.chatSid, ev.currentTarget.value);
+                try {
+                    await mChat.sendMessage(props.chatSid, ev.currentTarget.value);
+                }
+                catch (e) {
+                    if (e.toString().toLowerCase().includes("unauthorized")) {
+                        // TODO: Disable sending
+                        logger.error("Permission denied.");
+                    }
+                    else {
+                        // TODO: Show error to user
+                        logger.error(e);
+                    }
+                }
             }
         }
     }
+
+    // TODO: Somehow detect whether we're going to be permitted to send messages
+    //       or not (maybe query our Twilio Backend), then hide the Send box if
+    //       we're not allowed.
 
     return <div className="chat-frame">
         <MessageList chatSid={props.chatSid} />
