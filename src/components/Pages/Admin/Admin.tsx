1340cee1c277b5069939408fe96ed04435d768b6
Ross Gardiner <github@public.rossng.eu>
2020-10-03 20:23:40 +0100

    Add loading spinners to admin page

--- src/components/Pages/Admin/Admin.tsx
+++ src/components/Pages/Admin/Admin.tsx
@@ -1,4 +1,3 @@
-import { _Role } from "@clowdr-app/clowdr-db-schema";
 import Parse from "parse";
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
@@ -27,6 +26,7 @@ export default function Admin(props: Props) {
     const loggedInUserProfile = useUserProfile();
     const conference = useConference();
     const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
+    const [isSending, setIsSending] = useState<boolean>(false);
 
     useHeading({
         title: "Admin",
@@ -37,6 +37,7 @@ export default function Admin(props: Props) {
     }, setIsAdmin, [loggedInUserProfile])
 
     async function doSendRegistrationEmails(data: SendRegistrationEmailsData): Promise<SendRegistrationEmailsResponse> {
+        setIsSending(true);
         let response = await Parse.Cloud.run("registration-send-emails", data) as SendRegistrationEmailsResponse;
         return response;
     }
@@ -45,20 +46,25 @@ export default function Admin(props: Props) {
         doSendRegistrationEmails({ sendOnlyUnsent: sendOnlyUnsent, conference: conference.id })
             .then(response => {
                 if (response.success) {
-                    addNotification(`Sent ${response.results.length} registration ${response.results.length === 1 ? "email" : "emails"}.`, "", "");
+                    addNotification(`Sent ${response.results.length} registration ${response.results.length === 1 ? "email" : "emails"}.`);
                 } else {
                     addError(`Failed to send ${response.results.filter(result => !result.success).length} of ${response.results.length} registration ${response.results.length === 1 ? "email" : "emails"}.`)
                 }
+                setIsSending(false);
+            })
+            .catch(_ => {
+                addError("Error when attempting to send registration emails.");
+                setIsSending(false);
             });
     }
 
     const adminPanel = <>
         <h2>Send initial registration emails</h2>
         <p>Send the first registration email to any users that have not yet been sent one.</p>
-        <p><button onClick={() => sendRegistrationEmails(true)}>Send initial registration emails</button></p>
+        <p>{isSending ? <LoadingSpinner message="Sending emails" /> : <button onClick={() => sendRegistrationEmails(true)}>Send initial registration emails</button>}</p>
         <h2>Send repeat registration emails</h2>
         <p>Send a repeat registration email to all users that have not yet registered. Includes users that have not yet received any registration email.</p>
-        <p><button onClick={() => sendRegistrationEmails(false)}>Send repeat registration emails</button></p>
+        <p>{isSending ? <LoadingSpinner message="Sending emails" /> : <button onClick={() => sendRegistrationEmails(false)}>Send repeat registration emails</button>}</p>
     </>;
 
     function contents() {
