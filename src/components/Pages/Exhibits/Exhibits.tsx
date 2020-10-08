d26247796c5f3d0bab901eb08e71ae9c22e4808e
Ross Gardiner <github@public.rossng.eu>
2020-10-08 03:55:45 +0100

    Continue building Exhibits

--- src/components/Pages/Exhibits/Exhibits.tsx
+++ src/components/Pages/Exhibits/Exhibits.tsx
@@ -1,10 +1,13 @@
-import { ProgramItem, ProgramItemAttachment } from "@clowdr-app/clowdr-db-schema";
+import { ProgramItem, ProgramItemAttachment, ProgramPerson } from "@clowdr-app/clowdr-db-schema";
 import { DataDeletedEventDetails, DataUpdatedEventDetails } from "@clowdr-app/clowdr-db-schema/build/DataLayer/Cache/Cache";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
 import useConference from "../../../hooks/useConference";
 import useDataSubscription from "../../../hooks/useDataSubscription";
 import useHeading from "../../../hooks/useHeading";
 import useSafeAsync from "../../../hooks/useSafeAsync";
+import AttachmentLink from "../Program/Event/AttachmentLink";
+import "./Exhibits.scss";
 
 interface ExhibitsProps {
 
@@ -13,20 +16,26 @@ interface ExhibitsProps {
 interface RenderData {
     programItems: Array<ProgramItem>;
     programItemAttachments: Map<string, Array<ProgramItemAttachment>>;
+    programPeople: Map<string, ProgramPerson | undefined>;
 }
 
 export default function Exhibits(props: ExhibitsProps) {
     const conference = useConference();
 
-    const [renderData, setRenderData] = useState<RenderData>({ programItems: [], programItemAttachments: new Map() });
+    const [renderData, setRenderData] = useState<RenderData>({ programItems: [], programItemAttachments: new Map(), programPeople: new Map() });
 
     useHeading("Exhibition");
+
+    // Note: we don't bother keeping ProgramPersons' details up to date
+
+    // TODO: fetch new authors details as they arrive
 
     // Fetch initial render data (program items and their attachments)
     useSafeAsync(
         async () => {
-
             let programItems = (await ProgramItem.getAll(conference.id)).filter(programItem => programItem.exhibit);
+            let authors = (await Promise.all(programItems.map(async programItem => await programItem.authorPerons))).flat();
+            let programPeople = new Map(authors.map(author => [author.id, author]))
 
             async function programItemToAttachmentsEntry(programItem: ProgramItem): Promise<[string, ProgramItemAttachment[]]> {
                 return [programItem.id, await programItem.attachments];
@@ -36,6 +45,7 @@ export default function Exhibits(props: ExhibitsProps) {
             return {
                 programItems,
                 programItemAttachments,
+                programPeople,
             };
         },
         setRenderData, [conference.id]);
@@ -49,11 +59,9 @@ export default function Exhibits(props: ExhibitsProps) {
             let updatedProgramItem = ev.object as ProgramItem;
             if (idx === -1 && updatedProgramItem.exhibit) {
                 newProgramItems.push(updatedProgramItem);
-                //setDirtyItems(dirty => [...dirty, updatedProgramItem.id]);
             } else {
                 if (updatedProgramItem.exhibit) {
                     newProgramItems.splice(idx, 1, updatedProgramItem);
-                    //setDirtyItems(dirty => [...dirty, updatedProgramItem.id]);
                 } else {
                     newProgramItems.splice(idx, 1);
                     newProgramItemAttachments.delete(updatedProgramItem.id);
@@ -62,6 +70,7 @@ export default function Exhibits(props: ExhibitsProps) {
             return {
                 programItems: newProgramItems,
                 programItemAttachments: newProgramItemAttachments,
+                programPeople: renderData.programPeople,
             }
         });
     }, []);
@@ -73,6 +82,7 @@ export default function Exhibits(props: ExhibitsProps) {
             return {
                 programItems: renderData.programItems.filter(item => item.id !== ev.objectId),
                 programItemAttachments: newProgramItemAttachments,
+                programPeople: renderData.programPeople,
             }
         });
     }, []);
@@ -85,20 +95,19 @@ export default function Exhibits(props: ExhibitsProps) {
             const newProgramItemAttachmentsMap = new Map<string, ProgramItemAttachment[]>();
             let updatedProgramItemAttachment = ev.object as ProgramItemAttachment;
 
+            // First, remove the attachment from any existing items
             for (let [programItemId, programItemAttachments] of renderData.programItemAttachments.entries()) {
-                const newProgramItemAttachments = Array.from(programItemAttachments ?? []);
-                const idx = programItemAttachments.findIndex(x => x.id === updatedProgramItemAttachment.id);
-                if (idx === -1) {
-                    newProgramItemAttachments.push(updatedProgramItemAttachment);
-                } else {
-                    newProgramItemAttachments.splice(idx, 1, updatedProgramItemAttachment);
-                }
+                const newProgramItemAttachments = programItemAttachments.filter(attachment => updatedProgramItemAttachment.id !== attachment.id);
                 newProgramItemAttachmentsMap[programItemId] = newProgramItemAttachments;
             }
+
+            // Then add the attachment back to its current item
+            newProgramItemAttachmentsMap[updatedProgramItemAttachment.programItemId] = updatedProgramItemAttachment;
 
             return {
                 programItems: renderData.programItems,
                 programItemAttachments: newProgramItemAttachmentsMap,
+                programPeople: renderData.programPeople,
             };
         });
     }, []);
@@ -114,13 +123,24 @@ export default function Exhibits(props: ExhibitsProps) {
             return {
                 programItems: renderData.programItems,
                 programItemAttachments: newProgramItemAttachmentsMap,
+                programPeople: renderData.programPeople,
             };
         });
     }, []);
 
     useDataSubscription("ProgramItemAttachment", onProgramItemAttachmentUpdated, onProgramItemAttachmentDeleted, renderData.programItems.length === 0, conference);
 
-    return <>
-        {}
-    </>
+    return <section aria-labelledby="page-title" tabIndex={0} className="exhibits-page">
+        <div className="exhibits">
+            {renderData.programItems.map(programItem => {
+                let attachments = renderData.programItemAttachments.get(programItem.id);
+                return <article className="exhibit" aria-labelledby={`exhibit-${programItem.id}__title`} key={programItem.id}>
+                    <h2 id={`exhibit-${programItem.id}__title`}>{programItem.title}</h2>
+                    <p>{programItem.abstract}</p>
+                    {attachments ? attachments.map(x => <AttachmentLink key={x.id} attachment={x} />) : <></>}
+                    <Link to={`/item/${programItem.id}`} className="button">View</Link>
+                </article>;
+            })}
+        </div>
+    </section>
 }
