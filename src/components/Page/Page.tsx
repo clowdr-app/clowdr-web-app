9206ee39aaac853281b09b33e1c4bd8b24dba0df
Ross Gardiner <github@public.rossng.eu>
2020-11-08 18:37:04 +0000

    Revert "Eliminate missing keys error in router"
    This reverts commit 4b542c87cce015aec7c37a780a6a352a900396f4.

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -145,13 +145,11 @@ function Page(props: Props) {
     // build or runtime, just a single double quote character to mess with
     // your mind.
     const footerRoutes = useMemo(
-        () => (
-            <Switch>
-                <Route path="/about" component={About} />,
-                <Route path="/legal" component={Legal} />,
-                <Route path="/help" component={Help} />,
-            </Switch>
-        ),
+        () => [
+            <Route path="/about" component={About} />,
+            <Route path="/legal" component={Legal} />,
+            <Route path="/help" component={Help} />,
+        ],
         []
     );
 
