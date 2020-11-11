b74349266af3890049cfdf77875f6cfdefba75f9
Ed Nutting <github@ednutting.com>
2020-11-11 01:05:25 +0000

    Stretch videos in video grid to the size of their cell (`object-fit: contain`)

--- src/components/Video/VideoFrontend/components/VideoTrack/VideoTrack.tsx
+++ src/components/Video/VideoFrontend/components/VideoTrack/VideoTrack.tsx
@@ -6,44 +6,45 @@ import useMediaStreamTrack from '../../hooks/useMediaStreamTrack/useMediaStreamT
 import useVideoTrackDimensions from '../../hooks/useVideoTrackDimensions/useVideoTrackDimensions';
 
 const Video = styled('video')({
-  maxWidth: "100%",
-  maxHeight: "100%",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    objectFit: "contain"
 });
 
 interface VideoTrackProps {
-  track: IVideoTrack;
-  isLocal?: boolean;
-  priority?: Track.Priority | null;
+    track: IVideoTrack;
+    isLocal?: boolean;
+    priority?: Track.Priority | null;
 }
 
 export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps) {
-  const ref = useRef<HTMLVideoElement>(null!);
-  const mediaStreamTrack = useMediaStreamTrack(track);
-  const dimensions = useVideoTrackDimensions(track);
-  const isPortrait = (dimensions?.height ?? 0) > (dimensions?.width ?? 0);
+    const ref = useRef<HTMLVideoElement>(null!);
+    const mediaStreamTrack = useMediaStreamTrack(track);
+    const dimensions = useVideoTrackDimensions(track);
+    const isPortrait = (dimensions?.height ?? 0) > (dimensions?.width ?? 0);
 
-  useEffect(() => {
-    const el = ref.current;
-    el.muted = true;
-    if (track.setPriority && priority) {
-      track.setPriority(priority);
-    }
-    track.attach(el);
-    return () => {
-      track.detach(el);
-      if (track.setPriority && priority) {
-        // Passing `null` to setPriority will set the track's priority to that which it was published with.
-        track.setPriority(null);
-      }
+    useEffect(() => {
+        const el = ref.current;
+        el.muted = true;
+        if (track.setPriority && priority) {
+            track.setPriority(priority);
+        }
+        track.attach(el);
+        return () => {
+            track.detach(el);
+            if (track.setPriority && priority) {
+                // Passing `null` to setPriority will set the track's priority to that which it was published with.
+                track.setPriority(null);
+            }
+        };
+    }, [track, priority]);
+
+    // The local video track is mirrored if it is not facing the environment.
+    const isFrontFacing = mediaStreamTrack?.getSettings().facingMode !== 'environment';
+    const style = {
+        transform: isLocal && isFrontFacing ? 'rotateY(180deg)' : '',
+        objectFit: isPortrait || track.name.includes('screen') ? ('contain' as const) : ('cover' as const),
     };
-  }, [track, priority]);
 
-  // The local video track is mirrored if it is not facing the environment.
-  const isFrontFacing = mediaStreamTrack?.getSettings().facingMode !== 'environment';
-  const style = {
-    transform: isLocal && isFrontFacing ? 'rotateY(180deg)' : '',
-    objectFit: isPortrait || track.name.includes('screen') ? ('contain' as const) : ('cover' as const),
-  };
-
-  return <Video ref={ref} style={style} />;
+    return <Video ref={ref} style={style} />;
 }
